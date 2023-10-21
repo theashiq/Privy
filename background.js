dadb9eb03ab2dbf344aa1047f39b5ef1846441be
Mac Mini <>
2023-10-21 22:34:26 +0600

    Completed opening URL in private window and searching in private window

--- background.js
+++ background.js
@@ -1,7 +1,41 @@
+/** Start of - Extension action button functionalities */
 chrome.action.onClicked.addListener((tab) => {
-    openUrlInAPrivateWindow(tab.url)
+    chrome.windows.create(
+        { 
+            "url": tab.url, 
+            "incognito": true 
+        }
+    );
 });
 
-function openUrlInAPrivateWindow(url){
-    chrome.windows.create({"url": url, "incognito": true});
-}
+chrome.tabs.onUpdated.addListener(function (tabId , info, tab) {
+    if(tab.incognito){
+        chrome.action.disable(tab.id);
+    }
+});
+/** End of - Extension action button functionalities */
+
+/** Start of - Extension context menu functionalities */
+chrome.runtime.onInstalled.addListener(async () => {
+    chrome.contextMenus.create({
+        id: "privysearch",
+        title: "Search Privately",
+        type: 'normal',
+        contexts: ['selection']
+    });
+});
+
+chrome.contextMenus.onClicked.addListener((item, tab) => {
+    chrome.windows.create({
+        "incognito": true
+    },
+    async (window) => {
+        chrome.search.query(
+            {
+                tabId: window.tabs[0].id,
+                text: item.selectionText
+            }
+        )
+    });
+});
+/** End of - Extension context menu functionalities */
